9d00ccb8d05b3a3aaa052befcac1f6b50859e733
lightzane <jp.eaguilar@gmail.com>
2024-03-24 08:09:11 +0800

    update basename for hash router

--- src/constants/basename.ts
+++ src/constants/basename.ts
@@ -1,3 +1,7 @@
-export const basename = import.meta.env.PROD // vite-env.d.ts
-  ? '/draft-test-labs/'
-  : '';
+/** Use this for **Browser Router** */
+// export const basename = import.meta.env.PROD // vite-env.d.ts
+//   ? '/draft-test-labs/'
+//   : '';
+
+/** Use this for **Hash Router** */
+export const basename = '';
